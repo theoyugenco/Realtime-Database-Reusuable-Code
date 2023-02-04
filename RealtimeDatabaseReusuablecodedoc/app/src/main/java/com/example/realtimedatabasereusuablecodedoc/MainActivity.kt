1e16ecd3374253f55fb50831d92ea054490ac7bb
theoyugenco <103534036+theoyugenco@users.noreply.github.com>
2023-02-04 11:37:42 -0800

    Created a Registration page (former MainActivity.kt) Made the MainActivity.kt the login page Able to switch activities from MainActivity.kt -> Registration.kt Able to add Users into Realtime Database

--- RealtimeDatabaseReusuablecodedoc/app/src/main/java/com/example/realtimedatabasereusuablecodedoc/MainActivity.kt
+++ RealtimeDatabaseReusuablecodedoc/app/src/main/java/com/example/realtimedatabasereusuablecodedoc/MainActivity.kt
@@ -1,46 +1,26 @@
 package com.example.realtimedatabasereusuablecodedoc
 
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import com.example.realtimedatabasereusuablecodedoc.databinding.ActivityMainBinding
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
+import android.content.Intent
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private lateinit var dbase: DatabaseReference
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        binding.registerBtn. setOnClickListener(){
-            val fName = binding.firstName.text.toString()
-            val lName = binding.lastName.text.toString()
-            val age = binding.age.text.toString()
-            val uName = binding.userName.text.toString()
-
-            dbase = FirebaseDatabase.getInstance().getReference("Users")
-            val UserDC = UserDC(fName, lName, age, uName)
-            dbase.child(uName).setValue(UserDC).addOnSuccessListener {
-                //The (registering) User successfully registered (valid inputs)
-                //Thus we clear the input text provided to sanitize (confused EXACTLY why)
-                binding.firstName.text.clear()
-                binding.lastName.text.clear()
-                binding.age.text.clear()
-                binding.userName.text.clear()
-
-                Toast.makeText(this, "Your account has been successfully created and registered.", Toast.LENGTH_SHORT).show()
-
-            }.addOnFailureListener(){
-                Toast.makeText(this, "Failure to create and register account.  Please try again.", Toast.LENGTH_SHORT).show()
-            }
+        binding.tvSignUp.setOnClickListener()
+        {
+            val intent = Intent(this, Registration::class.java)
+            startActivity(intent)
         }
-
-
     }
 }
