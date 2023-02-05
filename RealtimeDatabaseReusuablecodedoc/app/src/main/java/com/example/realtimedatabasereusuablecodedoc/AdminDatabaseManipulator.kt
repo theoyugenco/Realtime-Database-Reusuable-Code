6dbe75df16299bfb9ab6096223e5936466db4b8d
theoyugenco <103534036+theoyugenco@users.noreply.github.com>
2023-02-04 21:50:53 -0800

    Revert "Build: Completed creating, deleting, editing, reading Users from Realtime Database (RTD)"
    This reverts commit f27e2f51cdfd7ebda5b30f832b335d7f33f81852.

--- RealtimeDatabaseReusuablecodedoc/app/src/main/java/com/example/realtimedatabasereusuablecodedoc/AdminDatabaseManipulator.kt
+++ RealtimeDatabaseReusuablecodedoc/app/src/main/java/com/example/realtimedatabasereusuablecodedoc/AdminDatabaseManipulator.kt
@@ -35,93 +35,11 @@ class AdminDatabaseManipulator : AppCompatActivity() {
 
         }
 
-        binding.btnEdit.setOnClickListener(){
-            val uName : String? = binding.etUserName.text.toString()
-            val fName : String? = binding.etFirstName.text.toString()
-            val lName : String? = binding.etLastName.text.toString()
-            val age : String? = binding.etAge.text.toString()
-
-            if (uName.isNullOrEmpty()){
-                Toast.makeText(this, "You must provide the UserName of the User you wish to edit!", Toast.LENGTH_SHORT).show()
-            }
-            else if ((fName.isNullOrEmpty()) && (lName.isNullOrEmpty()) && (age.isNullOrEmpty())){
-                Toast.makeText(this, "You must provide at least one parameter to edit!", Toast.LENGTH_SHORT).show()
-            }
-            else{
-                editData(uName, fName, lName, age)
-            }
-        }
-
-        binding.btnDelete.setOnClickListener(){
-            val uName : String = binding.etUserName.text.toString()
-
-            if (uName.isNotEmpty()){
-                deleteData(uName)
-            }
-            else{
-                Toast.makeText(this, "Please enter the Username", Toast.LENGTH_SHORT).show()
-            }
-        }
 
 
 
-    }
-
-    private fun editData(uName: String?, fName: String?, lName: String?, age: String?) {
-        var firstName : String? = fName
-        var lastName : String? = lName
-        var age : String? = age
-        database = FirebaseDatabase.getInstance().getReference("Users")
-        database.child(uName!!).get().addOnSuccessListener {
-            if(it.exists()) {
-                //If a node/entry of that specific UserName exists
-                if (firstName.isNullOrEmpty()) {
-                    firstName = it.child("fname").value as String?
-                }
-                if (lastName.isNullOrEmpty()) {
-                    lastName = it.child("lname").value as String?
-                }
-                if (age.isNullOrEmpty()) {
-                    age = it.child("age").value as String?
-                }
-
-                val updated_user = mapOf<String, String>(
-                    "fname" to firstName!!,
-                    "lname" to lastName!!,
-                    "age" to age!!
-                )
-                database.child(uName!!).updateChildren(updated_user).addOnSuccessListener {
-                    binding.etUserName.text.clear()
-                    binding.etFirstName.text.clear()
-                    binding.etLastName.text.clear()
-                    binding.etAge.text.clear()
-                    Toast.makeText(this, "Successfully updated the User's information\nUpdated Profile showcased below!", Toast.LENGTH_SHORT).show()
-                    readData(uName)
-                }.addOnFailureListener(){
-                    Toast.makeText(this, "Unsuccessfully in updating User's information", Toast.LENGTH_SHORT).show()
-                }
-            }
-            //If they provided a Username that does not exist (no such entry or node with that Username)
-            else{
-                Toast.makeText(this, "A User with that UserName does NOT exist!", Toast.LENGTH_SHORT).show()
-            }
-
-        }.addOnFailureListener(){
-            Toast.makeText(this, "Unable to read the User's data for editting!", Toast.LENGTH_SHORT).show()
-        }
 
 
-
-    }
-
-    private fun deleteData(uName: String) {
-        database = FirebaseDatabase.getInstance().getReference("Users")
-        database.child(uName).removeValue().addOnSuccessListener {
-            Toast.makeText(this, "Successfully Deleted User!", Toast.LENGTH_SHORT).show()
-
-        }.addOnFailureListener(){
-            Toast.makeText(this, "Failed to Delete User", Toast.LENGTH_SHORT).show()
-        }
     }
 
     private fun readData(uName: String) {
