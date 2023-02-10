cdb0cf051144a068d22443c27b544cc34f5c2d86
KennethValero <54961992+KennethValero@users.noreply.github.com>
2023-02-09 16:28:44 -0800

    added class for messages in chat window

--- RealtimeDatabaseReusuablecodedoc/app/src/main/java/com/example/realtimedatabasereusuablecodedoc/Message.kt
+++ RealtimeDatabaseReusuablecodedoc/app/src/main/java/com/example/realtimedatabasereusuablecodedoc/Message.kt
@@ -1,4 +1,13 @@
 package com.example.realtimedatabasereusuablecodedoc
 
 class Message {
+    var message: String? = null
+    var senderEmail: String? = null
+
+    constructor(){}
+
+    constructor(message: String?, email: String?) {
+        this.message = message
+        this.senderEmail = email
+    }
 }
