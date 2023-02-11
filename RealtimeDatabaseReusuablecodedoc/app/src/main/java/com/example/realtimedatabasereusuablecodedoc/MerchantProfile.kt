e9d807d866c4c7fbd3a35590909ccf37dba544b0
Krisna Kieng <krisnakieng@gmail.com>
2023-02-10 20:01:31 -0800

    Merchant Profile

---
+++ RealtimeDatabaseReusuablecodedoc/app/src/main/java/com/example/realtimedatabasereusuablecodedoc/MerchantProfile.kt
@@ -0,0 +1,11 @@
+package com.example.realtimedatabasereusuablecodedoc
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MerchantProfile : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_merchant_profile)
+    }
+}
