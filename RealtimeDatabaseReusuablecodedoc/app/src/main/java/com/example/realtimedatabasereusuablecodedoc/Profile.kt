e49d3615ff733d9630fb6331119262f38be435de
KennethValero <54961992+KennethValero@users.noreply.github.com>
2023-02-10 19:47:13 -0800

    adding temporary button for chat testing

--- RealtimeDatabaseReusuablecodedoc/app/src/main/java/com/example/realtimedatabasereusuablecodedoc/Profile.kt
+++ RealtimeDatabaseReusuablecodedoc/app/src/main/java/com/example/realtimedatabasereusuablecodedoc/Profile.kt
@@ -16,6 +16,7 @@ class Profile : AppCompatActivity() {
 
     private lateinit var binding: ActivityProfileBinding
     private lateinit var database: DatabaseReference
+    private lateinit var chatButton: ImageView
     private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,6 +26,7 @@ class Profile : AppCompatActivity() {
         setContentView(binding.root)
 
         auth = FirebaseAuth.getInstance()
+        chatButton = findViewById(R.id.messagingButton)
         //val test = auth.currentUser.
         database = FirebaseDatabase.getInstance().getReference("Users")
         val currentUserUID = auth.currentUser!!.uid
@@ -42,6 +44,11 @@ class Profile : AppCompatActivity() {
                 binding.tvName.text = name.toString()
 
             }
+        }
+
+        chatButton.setOnClickListener {
+            val intent = Intent(this, ChatSearch::class.java)
+            startActivity(intent)
         }
 
 
