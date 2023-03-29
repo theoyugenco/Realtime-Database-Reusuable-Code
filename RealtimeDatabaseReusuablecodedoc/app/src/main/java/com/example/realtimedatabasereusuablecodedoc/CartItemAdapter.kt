127b46784f5f1f0395eb6c3a90afd46944e98188
KennethValero <54961992+KennethValero@users.noreply.github.com>
2023-03-28 17:13:19 -0700

    setting up classes for cart recyclerview

---
+++ RealtimeDatabaseReusuablecodedoc/app/src/main/java/com/example/realtimedatabasereusuablecodedoc/CartItemAdapter.kt
@@ -0,0 +1,4 @@
+package com.example.realtimedatabasereusuablecodedoc
+
+class CartItemAdapter {
+}
---
+++ RealtimeDatabaseReusuablecodedoc/app/src/main/java/com/example/realtimedatabasereusuablecodedoc/OrderItemDC.kt
@@ -0,0 +1,4 @@
+package com.example.realtimedatabasereusuablecodedoc
+
+data class OrderItemDC(val name: String? = null,
+                       val price: String? = null)
