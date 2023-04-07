312176ea5d15618a7a9d9a22210eea2b569e8fa8
KennethValero <54961992+KennethValero@users.noreply.github.com>
2023-04-07 15:47:31 -0700

    converting coupon data classes to kotlin

---
+++ RealtimeDatabaseReusuablecodedoc/app/src/main/java/com/example/realtimedatabasereusuablecodedoc/GeneralCoupon.kt
@@ -0,0 +1,61 @@
+package com.example.realtimedatabasereusuablecodedoc
+
+data class GeneralCoupon(var discountedPrice: Double? = null,
+                         var quantityNeeded: Double? = null,
+                         var usesBeforeExpiration: Int = 0,
+                         var currentUses: Int = 0,
+                         var expirationDate: String? = null
+) : ListItem {
+    fun getDiscountedPrice(): Double {
+        return discountedPrice!!
+    }
+    @JvmName("setDiscountedPrice1")
+    fun setDiscountedPrice(discountedPrice: Double?) {
+        this.discountedPrice = discountedPrice
+    }
+
+    @JvmName("getQuantityNeeded1")
+    fun getQuantityNeeded(): Double? {
+        return quantityNeeded
+    }
+
+    @JvmName("setQuantityNeeded1")
+    fun setQuantityNeeded(quantityNeeded: Double?) {
+        this.quantityNeeded = quantityNeeded
+    }
+
+    @JvmName("getUsesBeforeExpiration1")
+    fun getUsesBeforeExpiration(): Int {
+        return usesBeforeExpiration
+    }
+
+    @JvmName("setUsesBeforeExpiration1")
+    fun setUsesBeforeExpiration(usesBeforeExpiration: Int) {
+        this.usesBeforeExpiration = usesBeforeExpiration
+    }
+
+    @JvmName("getCurrentUses1")
+    fun getCurrentUses(): Int {
+        return currentUses
+    }
+
+    @JvmName("setCurrentUses1")
+    fun setCurrentUses(currentUses: Int) {
+        this.currentUses = currentUses
+    }
+
+    @JvmName("getExpirationDate1")
+    fun getExpirationDate(): String? {
+        return expirationDate
+    }
+
+    @JvmName("setExpirationDate1")
+    fun setExpirationDate(expirationDate: String?) {
+        this.expirationDate = expirationDate
+    }
+
+    override fun getListItemType(): Int {
+        return ListItem.Type.GeneralCoupon.ordinal
+    }
+}
+
